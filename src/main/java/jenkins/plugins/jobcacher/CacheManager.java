891cfd63fe12e8d0f2f350362dd2fe5fc14129c8
David van Laatum <david@vanlaatum.id.au>
2017-06-27 20:49:41 +0930

    Don't add multiple actions to run to prevent multi entries in the menu

--- src/main/java/jenkins/plugins/jobcacher/CacheManager.java
+++ src/main/java/jenkins/plugins/jobcacher/CacheManager.java
@@ -97,6 +97,8 @@ public class CacheManager {
         }
 
         // Add a build action so that users can navigate the cache stored on master through UI
-        run.addAction(new CacheBuildLastAction(caches));
+        if(run.getAction ( CacheBuildLastAction.class ) == null) {
+            run.addAction ( new CacheBuildLastAction ( caches ) );
+        }
     }
 }
