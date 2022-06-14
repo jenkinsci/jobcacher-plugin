3360c583fdfe391e00fd20b58d92862a3c38689a
repolevedavaj <jonas.bamberger@gmail.com>
2022-06-14 21:18:57 +0200

    Add config roundtrip test for cache wrapper form

---
+++ src/test/java/jenkins/plugins/jobcacher/ArbitraryFileCacheWrapperTest.java
@@ -0,0 +1,47 @@
+package jenkins.plugins.jobcacher;
+
+import com.sun.tools.javac.util.List;
+import hudson.model.FreeStyleProject;
+import jenkins.plugins.jobcacher.ArbitraryFileCache.CompressionMethod;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArbitraryFileCacheWrapperTest {
+
+    @Rule
+    public JenkinsRule jenkinsRule = new JenkinsRule();
+
+    @Test
+    public void testArbitraryFileCacheForm() throws Exception {
+        FreeStyleProject project = createProjectWithFullyConfiguredArbitraryFileCache("test");
+        String projectConfigXml = project.getConfigFile().asString();
+
+        jenkinsRule.configRoundtrip(project);
+        String projectConfigXmlAfterConfigRoundtrip = project.getConfigFile().asString();
+
+        assertThat(projectConfigXmlAfterConfigRoundtrip).isEqualTo(projectConfigXml);
+    }
+
+    private FreeStyleProject createProjectWithFullyConfiguredArbitraryFileCache(String name) throws IOException {
+        ArbitraryFileCache cache = new ArbitraryFileCache("path", "includes", "excludes");
+        cache.setCacheValidityDecidingFile("cacheValidityDecidingFile");
+        cache.setCompressionMethod(CompressionMethod.TARGZ);
+        cache.setUseDefaultExcludes(false);
+
+        CacheWrapper cacheWrapper = new CacheWrapper(999, List.of(cache));
+        cacheWrapper.setDefaultBranch("develop");
+
+        FreeStyleProject project = jenkinsRule.createProject(FreeStyleProject.class, name);
+        project.setDescription("description");
+        project.getBuildWrappersList().add(cacheWrapper);
+        project.setAssignedLabel(null);
+
+        return project;
+    }
+
+}
