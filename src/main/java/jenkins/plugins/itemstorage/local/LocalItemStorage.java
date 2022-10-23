c4e3d3adb7afca34048f31f9d4d391144c1b632f
repolevedavaj <jonas.bamberger@gmail.com>
2022-10-23 20:12:40 +0200

    Allow using a custom path while storing the items on the Jenkins controller

--- src/main/java/jenkins/plugins/itemstorage/local/LocalItemStorage.java
+++ src/main/java/jenkins/plugins/itemstorage/local/LocalItemStorage.java
@@ -28,13 +28,20 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.model.Item;
+import hudson.model.listeners.ItemListener;
+import jenkins.plugins.itemstorage.GlobalItemStorage;
 import jenkins.plugins.itemstorage.ItemStorage;
 import jenkins.plugins.itemstorage.ItemStorageDescriptor;
 import jenkins.plugins.itemstorage.Messages;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
- * Implementation of Item Storage that stores data on the Jenkins controller within the existing job folder.
+ * Implementation of Item Storage that stores data on the Jenkins controller within the existing job folder or in custom path.
  *
  * @author Peter Hayes
  */
@@ -42,23 +49,49 @@ public class LocalItemStorage extends ItemStorage<LocalObjectPath> {
 
     private static final long serialVersionUID = 1L;
 
+    private static final Logger LOG = Logger.getLogger(LocalItemStorage.class.getName());
+
+    private String root;
+
     @DataBoundConstructor
     public LocalItemStorage() {
     }
 
+    @DataBoundSetter
+    public void setRoot(String root) {
+        this.root = root;
+    }
+
+    public String getRoot() {
+        return root;
+    }
+
     @Override
     public LocalObjectPath getObjectPath(Item item, String path) {
-        return new LocalObjectPath(new FilePath(item.getRootDir()).child(path));
+        return new LocalObjectPath(getItemRoot(item).child(path));
     }
 
     @Override
     public LocalObjectPath getObjectPathForBranch(Item item, String path, String branch) {
-        FilePath parent = new FilePath(item.getRootDir()).getParent();
+        FilePath parent = getItemRoot(item).getParent();
         if (parent == null) {
             return null;
         }
+
         FilePath branchPath = parent.child(branch);
         return new LocalObjectPath(branchPath.child(path));
+    }
+
+    private FilePath getItemRoot(Item item) {
+        if (root != null) {
+            return getItemRoot(item.getFullName());
+        } else {
+            return new FilePath(item.getRootDir());
+        }
+    }
+
+    private FilePath getItemRoot(String itemFullName) {
+        return new FilePath(new File(root)).child(itemFullName);
     }
 
     @Extension
@@ -69,4 +102,56 @@ public class LocalItemStorage extends ItemStorage<LocalObjectPath> {
             return Messages.LocalItemStorage_DisplayName();
         }
     }
+
+    @Extension(optional = true)
+    public static final class LocalItemListener extends ItemListener {
+
+        @Override
+        public void onDeleted(Item item) {
+            LocalItemStorage storage = lookupStorage();
+            if (storage == null || storage.root == null) {
+                return;
+            }
+
+            try {
+                storage.getItemRoot(item).deleteRecursive();
+            } catch (IOException | InterruptedException e) {
+                if (e instanceof InterruptedException) {
+                    Thread.currentThread().interrupt();
+                }
+
+                LOG.warning("failed to delete item storage for " + item.getFullName() + ": " + e.getMessage());
+            }
+        }
+
+        @Override
+        public void onLocationChanged(Item item, String oldFullName, String newFullName) {
+            LocalItemStorage storage = lookupStorage();
+            if (storage == null || storage.root == null) {
+                return;
+            }
+
+            try {
+                storage.getItemRoot(oldFullName).renameTo(storage.getItemRoot(newFullName));
+            } catch (IOException | InterruptedException e) {
+                if (e instanceof InterruptedException) {
+                    Thread.currentThread().interrupt();
+                }
+
+                LOG.warning("failed to rename item storage for " + item.getFullName() + ": " + e.getMessage());
+            }
+        }
+
+        private static LocalItemStorage lookupStorage() {
+            ItemStorage<?> storage = GlobalItemStorage.get().getStorage();
+
+            if (storage instanceof LocalItemStorage) {
+                return (LocalItemStorage) storage;
+            } else {
+                return null;
+            }
+        }
+
+    }
+
 }
